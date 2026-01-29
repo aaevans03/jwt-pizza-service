7fa44c0703d86bc2980bc94e2a23924b5b95afa1
aaevans03 <aaevans03@gmail.com>
2026-01-29 12:49:02 -0700

    [test] authRouter: register

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -5,13 +5,25 @@ const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
 
 beforeAll(async () => {
-  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  testUser.email = randomName() + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
   expectValidJwt(testUserAuthToken);
 });
 
-test('login', async () => {
+test('Register', async () => {
+    const registerRes = await request(app).post('/api/auth').send(testUser);
+
+    expect(registerRes.status).toBe(200);
+    expectValidJwt(registerRes.body.token);
+
+    const expectedUser = { name: 'pizza diner', email: testUser.email, roles: [{ role: 'diner' }] };
+    expect(registerRes.body.user).toMatchObject(expectedUser);
+
+    console.log(registerRes.body);
+});
+
+test('Login', async () => {
   const loginRes = await request(app).put('/api/auth').send(testUser);
   expect(loginRes.status).toBe(200);
   expectValidJwt(loginRes.body.token);
@@ -24,3 +36,8 @@ test('login', async () => {
 function expectValidJwt(potentialJwt) {
   expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 }
+
+function randomName() {
+    return Math.random().toString(36).substring(2, 12);
+}
+
