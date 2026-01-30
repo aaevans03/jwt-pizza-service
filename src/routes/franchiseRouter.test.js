2d00dc40b651ccbb5b805c4c41c48b6107c4ca75
aaevans03 <aaevans03@gmail.com>
2026-01-29 22:23:20 -0700

    [test] Get user franchises

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -22,6 +22,7 @@ beforeAll(async () => {
     adminUser.name = adminUserResponse.name;
     adminUser.email = adminUserResponse.email;
     adminUser.password = adminUserResponse.password;
+    adminUser.id = adminUserResponse.id;
     
     const registerRes = await request(app).put('/api/auth').send({ email: adminUser.email, password: adminUser.password });
     adminUserAuthToken = registerRes.body.token;
@@ -30,7 +31,13 @@ beforeAll(async () => {
 
 afterAll(async() => {
     await DB.deleteTestingDatabase();
-})
+});
+
+beforeEach(async () => {
+    
+});
+
+// helper functions: create dummy franchises, delete all franchises
 
 test('Get Franchises', async () => {
     const response = await request(app).get('/api/franchise').set('Authorization', `Bearer ${adminUserAuthToken}`);
@@ -40,10 +47,36 @@ test('Get Franchises', async () => {
     expect(response.body).toHaveProperty('more');
 });
 
+test('Get User Franchises', async () => {
+    await request(app).post('/api/franchise').set('Authorization', `Bearer ${adminUserAuthToken}`).send({ name: 'My Franchise 1', admins: [{ email: adminUser.email }]});
+    await request(app).post('/api/franchise').set('Authorization', `Bearer ${adminUserAuthToken}`).send({ name: 'My Franchise 2', admins: [{ email: adminUser.email }]});
+
+    const response = await request(app).get('/api/franchise/' + adminUser.id).set('Authorization', `Bearer ${adminUserAuthToken}`);
+
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+    expect(response.body.length).toBeGreaterThan(0);
+
+    for (let i = 0; i < response.body.length; i++) {
+        const franchise = response.body[i];
+        expect(franchise).toHaveProperty('name');
+        expect(franchise).toHaveProperty('id');
+        expect(franchise).toHaveProperty('admins');
+
+        expect(franchise.admins).toContainEqual({ id: adminUser.id, name: adminUser.name, email: adminUser.email });
+
+        for (let j = 0; j < franchise.admins.length; j++) {
+            expect(franchise.admins[j]).toHaveProperty('id');
+            expect(franchise.admins[j]).toHaveProperty('name');
+            expect(franchise.admins[j]).toHaveProperty('email');
+        }
+        expect(franchise).toHaveProperty('stores');
+    }
+});
+
 test('Create Franchise', async () => {
     const response = await request(app).post('/api/franchise').set('Authorization', `Bearer ${adminUserAuthToken}`).send({ name: 'pizzaPocket', admins: [{ email: adminUser.email }]});
 
-    console.log(response.body);
     expect(response.status).toBe(200);
     expect(response.body.name).toBe('pizzaPocket');
     expect(response.body.admins).toEqual([ { email: adminUser.email, id: expect.any(Number), name: adminUser.name } ]);
