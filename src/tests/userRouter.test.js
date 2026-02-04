1dad841be5b2daf6df97df0c5b858683d2af9b40
aaevans03 <aaevans03@gmail.com>
2026-02-03 23:08:00 -0700

    [test] userRouter, admin update info of self

--- src/tests/userRouter.test.js
+++ src/tests/userRouter.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../service');
 const { DB } = require('../database/database');
 const { expectValidJwt } = require('./testHelper');
-const { createTestUser, createAdminUser, createFranchise, createStore } = require('./factories');
+const { createAdminUser } = require('./factories');
 
 let adminUser;
 let adminUserAuthToken;
@@ -25,6 +25,10 @@ beforeAll(async () => {
     expectValidJwt(adminUserAuthToken);
 });
 
+afterAll(async() => {
+    await DB.deleteTestingDatabase();
+});
+
 test('Get info for authenticated user', async () => {
     const response = await request(app).get('/api/user/me').set('Authorization', `Bearer ${adminUserAuthToken}`);
 
@@ -34,3 +38,23 @@ test('Get info for authenticated user', async () => {
     expect(response.body.email).toBe(adminUser.email);
     expect(response.body.roles).toEqual([{ role: 'admin' }]);
 });
+
+test('Admin update info of self', async () => {
+    const updatedName = 'Updated Admin Name';
+    const updatedEmail = 'updatedemail@jwt.com';
+    const updatedPassword = 'newpassword123';
+
+    const response = await request(app)
+        .put(`/api/user/${adminUser.id}`)
+        .set('Authorization', `Bearer ${adminUserAuthToken}`)
+        .send({ name: updatedName, email: updatedEmail, password: updatedPassword });
+
+    expect(response.status).toBe(200);
+    expect(response.body.user.id).toBe(adminUser.id);
+    expect(response.body.user.name).toBe(updatedName);
+    expect(response.body.user.email).toBe(updatedEmail);
+    expect(response.body.user.roles).toEqual([{ role: 'admin' }]);
+    expectValidJwt(response.body.token);
+});
+
+// FUTURE: Test-driven development with new userRouter endpoints
